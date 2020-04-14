190c93278c9ca2aeb74007210f27754f5ba84bb8
9names <60134748+9names@users.noreply.github.co>
2020-04-14 20:03:26 +1000

    Fix rust project folder

--- rustbuild.bat
+++ rustbuild.bat
@@ -1,6 +1,6 @@
 @REM UPDATE THIS TO YOUR INSTALL PATH!
 @set GNUPATH=C:\Cypress\PSoC Creator\4.3\PSoC Creator\import\gnu\arm\5.4.1
 @bindgen bindgen_wrappers.h --ctypes-prefix "cty" --use-core -o rust_project/src/bindings.rs -- --target=arm -mfloat-abi=soft -mcpu=cortex-m3 -mthumb -Icodegentemp -IGenerated_Source/PSoC5 -I"%GNUPATH%\arm-none-eabi\include"
-@pushd rust
+@pushd rust_project
 @cargo build
 @popd
